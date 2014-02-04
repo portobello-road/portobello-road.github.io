b8302de9c51d403fe6b45f3f7555e4da7607ce2a
Justin Lynn <j@jaesharp.com>
2014-02-03 22:53:19 -0800

    add github latest news and contribution bar

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+var month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+if($('#github')){
+    $.ajax({
+       url: "https://api.github.com/repos/portobello-road/barrow/commits",
+       dataType: 'jsonp',
+       success: function(json){
+           var last_commit = json.data[0];
+           if(!last_commit) return;
+
+           var timestamp = new Date(last_commit.commit.committer.date),
+               timestamp_serialised = [month[timestamp.getMonth()], timestamp.getDate() + ',', timestamp.getFullYear()].join(' ');
+
+           $('#github .date').text(timestamp_serialised);
+           $('#github .commit-name').html('Commit ' + last_commit.sha + ' &raquo;');
+           $('#github .commit-name').attr('href', "https://github.com/portobello-road/barrow/commit/" + last_commit.sha);
+       }
+    });
+}
